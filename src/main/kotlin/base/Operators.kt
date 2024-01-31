34f48cc436275f4fa87b7f9627da4ff9e1f1c21a
Anand Bose <anandbose16@gmail.com>
2024-01-31 23:01:21 +0530

    Add operator overloaded equivalents for add, subtract, multiply and divide to build expressions in a clear and concise manner

--- src/main/kotlin/base/Operators.kt
+++ src/main/kotlin/base/Operators.kt
@@ -18,6 +18,32 @@ fun divide(self: Expression, other: Double): Expression = Division(self, Constan
 
 fun variable(name: String, value: Double = 0.0) = VariableExpression(name, value)
 
-fun pow(self: Expression, other: Expression): Expression = Power(self, other)
+operator fun Expression.plus(other: Expression): Expression = Addition(this, other)
 
-fun pow(self: Expression, other: Double): Expression = Power(self, ConstantExpression(other))
+operator fun Expression.plus(other: Number): Expression = Addition(this, ConstantExpression(other.toDouble()))
+
+operator fun Number.plus(other: Expression): Expression = Addition(ConstantExpression(toDouble()), other)
+
+operator fun Expression.minus(other: Expression): Expression = Subtraction(this, other)
+
+operator fun Expression.minus(other: Number): Expression = Subtraction(this, ConstantExpression(other.toDouble()))
+
+operator fun Number.minus(other: Expression): Expression = Subtraction(ConstantExpression(toDouble()), other)
+
+operator fun Expression.times(other: Expression): Expression = Multiplication(this, other)
+
+operator fun Expression.times(other: Number): Expression = Multiplication(this, ConstantExpression(other.toDouble()))
+
+operator fun Number.times(other: Expression): Expression = Multiplication(ConstantExpression(toDouble()), other)
+
+operator fun Expression.div(other: Expression): Expression = Division(this, other)
+
+operator fun Expression.div(other: Number): Expression = Division(this, ConstantExpression(other.toDouble()))
+
+operator fun Number.div(other: Expression): Expression = Division(ConstantExpression(toDouble()), other)
+
+fun Expression.pow(other: Expression): Expression = Power(this, other)
+
+fun Expression.pow(other: Number): Expression = Power(this, ConstantExpression(other.toDouble()))
+
+fun Number.pow(other: Expression): Expression = Power(ConstantExpression(toDouble()), other)
